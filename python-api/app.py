62277b04c6c8f93b1d36216c13d1cdfcdf12c95f
Weggal <72097940+Weggal@users.noreply.github.com>
2024-09-26 12:10:23 +0200

    Update app.py

--- python-api/app.py
+++ python-api/app.py
@@ -3,34 +3,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# seule = [
-#     {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 5},
-#     {"id": 2, "name": "Australia", "capital": "Canberra", "area": 7617930},
-#     {"id": 3, "name": "Egypt", "capital": "Cairo", "area": 1010408},
-# ]
-
-# def _find_next_id():
-#     return max(country["id"] for country in seule) + 1
-
-# @app.get("/countries")
-# def get_countries():
-#     return jsonify(seule)
-
-# @app.post("/countries")
-# def add_country():
-#     if request.is_json:
-#         country = request.get_json()
-#         country["id"] = _find_next_id()
-#         countries.append(country)
-#         return country, 201
-#     return {"error": "Request must be JSON"}, 415
-
 stations = {
     "1" : "90",
     "2" : "75",
-    "3" : "80",
-    "4" : "50",
-    "5" : "60"
+    "3" : "50",
+    "4" : "70",
+    "5" : "65",
+    "6" : "80",
+    "7" : "50",
+    "8" : "60"
 }
 
 @app.route('/requestStation', methods=['GET'])
