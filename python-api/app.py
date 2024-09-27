589c9a3f64d8d2a93eb7095b80abc18dc9aac8bf
Herry <herrytco@gmail.com>
2024-09-27 10:00:07 +0200

    app icon

--- python-api/app.py
+++ python-api/app.py
@@ -1,7 +1,9 @@
 # app.py
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 stations = {
     "1" : "90",
